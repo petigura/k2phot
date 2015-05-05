3f5dce04bb676dbd0dcb5ef54ff55b9048f7689e
Erik Petigura <epetigura@berkeley.edu>
2015-05-05 15:44:00 -0700

    Removed machine-specific convenience scripts

--- bin/pull_k2photfiles.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -x
-rsync -avhz --progress dtn01:${NERSCPROJ}/www/k2photfiles ${K2PHOT_DIR}/ 
--- bin/push_k2photfiles.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -x
-rsync -avhz --progress ${K2PHOT_DIR}/k2photfiles dtn01:${NERSCPROJ}/www/
