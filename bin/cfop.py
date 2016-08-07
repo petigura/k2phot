77b940d55ab15afbbb2eee3f5a6f90cff7d8bf39
Erik Petigura <epetigura@berkeley.edu>
2016-08-07 10:07:32 -0700

    changes to bulk upload files

--- bin/cfop.py
+++ bin/cfop.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python 
+import matplotlib
+matplotlib.use('Agg')
 from argparse import ArgumentParser
 import pandas as pd
 from jinja2 import Environment, PackageLoader, FileSystemLoader
@@ -9,7 +12,7 @@ from matplotlib import pylab as plt
 
 K2_ARCHIVE=os.environ['K2_ARCHIVE']
 
-def cfop(run, starname):
+def cfop(run, starname, plots=False, lc=True):
     os.path.join(K2_ARCHIVE,'photometry/',run,'')
     outdir = '{}/photometry/{}/output/{}'.format(K2_ARCHIVE, run, starname)
 
@@ -18,36 +21,42 @@ def cfop(run, starname):
     phot = k2phot.phot.read_fits(fitsfn,'optimum')
     lcout = phot.lc['t cad fsap fbg fdt_t_rollmed fdt_t_roll_2D bgmask thrustermask fdtmask fmask'.split()]
 
-    env = Environment(loader=PackageLoader('k2phot', 'templates'))
-    template = env.get_template('lightcurve-csv-header.csv')
-    npix = phot.ap_weights.sum()
-    starname = phot.header['KEPLERID']
-    csvfn = "{}/{}.csv".format(outdir,starname)
+    if lc:
+        env = Environment(loader=PackageLoader('k2phot', 'templates'))
+        template = env.get_template('lightcurve-csv-header.csv')
+        npix = phot.ap_weights.sum()
+        starname = phot.header['KEPLERID']
+        csvfn = "{}/{}.csv".format(outdir,starname)
 
-    with open(csvfn,'w') as f:
-        s = template.render(npix=npix,epic=starname)
-        s += '\n' # need to add another return
-        f.writelines(s)
-        lcout.to_csv(f,index=False,float_format="%.6f")
+        with open(csvfn,'w') as f:
+            s = template.render(npix=npix,epic=starname)
+            s += '\n' # need to add another return
+            f.writelines(s)
+            lcout.to_csv(f,index=False,float_format="%.6f")
 
-    pnglist = []
-    pngfn = "{}/{}_0-aperture.png".format(outdir,starname)
-    pnglist.append(pngfn)
-    k2phot.plotting.phot.aperture(phot)
-    plt.gcf().savefig(pngfn,dpi=160)
-    plt.close()
+        print "cfop: created: {}".format(csvfn)
+            
+    if plots:
+        pnglist = []
+        pngfn = "{}/{}_0-aperture.png".format(outdir,starname)
+        pnglist.append(pngfn)
+        k2phot.plotting.phot.aperture(phot)
+        plt.gcf().savefig(pngfn,dpi=160)
+        plt.close()
 
-    pngfn = "{}/{}_5-fdt_t_rollmed.png".format(outdir,starname)
-    pnglist.append(pngfn)
-    k2phot.plotting.phot.detrend_t_rollmed(phot)
-    plt.gcf().savefig(pngfn,dpi=160)
-    plt.close()
+        pngfn = "{}/{}_5-fdt_t_rollmed.png".format(outdir,starname)
+        pnglist.append(pngfn)
+        k2phot.plotting.phot.detrend_t_rollmed(phot)
+        plt.gcf().savefig(pngfn,dpi=160)
+        plt.close()
 
-    pdffn = "{}/{}.pdf".format(outdir,starname)
-    files = pnglist + [pdffn]
-    cmd = 'convert ' + " ".join(files)
-    print cmd
-    os.system(cmd)
+        pdffn = "{}/{}.pdf".format(outdir,starname)
+        files = pnglist + [pdffn]
+        cmd = 'convert ' + " ".join(files)
+        print cmd
+        os.system(cmd)
+
+        print "cfop: created: {}".format(pdffn)
 
 def main():
     psr = ArgumentParser()
@@ -55,6 +64,7 @@ def main():
     psr.add_argument('starlist',type=str)
     psr.add_argument('njobs',type=int)
     psr.add_argument('jobid',type=int)
+    psr.add_argument('--plots', action='store_true')
 
     args = psr.parse_args()
     stars = pd.read_csv(args.starlist,names=['starname'])
@@ -65,7 +75,7 @@ def main():
     for idx in idxL:
         starname = stars.ix[idx].starname
         try: 
-            cfop(args.run, starname)
+            cfop(args.run, starname, plots=args.plots)
             print "cfop: created plots {}, {}".format(idx, starname)
         except IOError:
             print "cfop: failed {}, {}".format(idx, starname)
