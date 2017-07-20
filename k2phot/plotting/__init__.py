818b7024302ce078ce2f68c10de7ed0110d6f45b
Erik Petigura <petigura@caltech.edu>
2017-07-20 10:36:30 -0700

    added in missing suffix

--- k2phot/plotting/__init__.py
+++ k2phot/plotting/__init__.py
@@ -3,4 +3,4 @@ import phot
 import pipeline
 import finderchart
 
-PLOT_SUFFIXES = ['0-aperture','1-background','3-fdt_t_roll_2D','4-fdt_t_roll_2D_zoom','5-fdt_t_rollmed']
+PLOT_SUFFIXES = ['0-aperture','1-background','2-noise_vs_aperture_size','3-fdt_t_roll_2D','4-fdt_t_roll_2D_zoom','5-fdt_t_rollmed']
